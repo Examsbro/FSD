669504b09b6d1b514b1c16f1956b463a124088aa
Examsbro <157946667+Examsbro@users.noreply.github.com>
2024-07-07 11:44:04 +0530

    server.js

--- week-8/server.js
+++ week-8/server.js
@@ -1,22 +1,12 @@
-// Import express for creating API's endpoints
+
 const express = require("express");
 const db = require("./database.js");
 const {MongoClient} =require('mongodb');
 var token;
-
-// Import jwt for API's endpoints authentication
 const jwt = require("jsonwebtoken");
-
-// Creates an Express application, initiate
-// express top level function
 const app = express();
-
-// A port for serving API's
 const port = 3000;
-
-// Allow json data
 app.use(express.json());
-
 app.get('/',
     (req, res) => {
         res.sendFile(__dirname + '/index.html');
@@ -33,16 +23,9 @@ app.get('/login',
     (req, res) => {
         res.sendFile(__dirname + '/login.html');
     });
-
-// SignUp route
 app.post("/register", async(req, res) => {
-	// Get the name to the json body data
 	const name = req.body.name;
-
-	// Get the password to the json body data
 	const password = req.body.password;
-
-	// Get the profession to the json body data
 	const work = req.body.work;
 	tuple= await db.getData(name,password);
 		if(JSON.parse(tuple).length>0){
@@ -52,11 +35,7 @@ app.post("/register", async(req, res) => {
 			error: "Already registered",
 		});
 	} else {
-
-		// The jwt.sign method are used
-		// to create token
 		const token = jwt.sign(tuple, "secret");
-
 	let emp =
 	{
     	name: name,
@@ -64,7 +43,6 @@ app.post("/register", async(req, res) => {
     	password: password,
 		token:token
 	};
-
 	let result=await db.insertData(emp);
 	res.json({
 		signup: true,
@@ -73,28 +51,14 @@ app.post("/register", async(req, res) => {
 	});
 	}
 });
-
-
-// Login route
 app.post("/auth", async (req, res) => {
-	// Get the name to the json body data
-
 	const name = req.body.name;
 	console.log(name);
-
-	// Get the password to the json body data
 	const password = req.body.password;
 	console.log(password)
-
 	tuple=await db.getData(name,password);
-
 	if (JSON.parse(tuple).length>0) {
-
-		// The jwt.sign method are used
-		// to create token
 		const token = jwt.sign(tuple, "secret");
-
-		// Pass the data or token in response
 		res.json({
 			login: true,
 			token: token,
@@ -107,34 +71,21 @@ app.post("/auth", async (req, res) => {
 		});
 	}
 });
-
-// Verify route
 app.post("/verifyToken", (req, res) => {
-
-	// Get token value to the json body
 	const token = req.body.token;
-
-	// If the token is present
 	if (token) {
-
-		// Verify the token using jwt.verify method
 		const decode = jwt.verify(token, "secret");
-
-		// Return response with decode data
 		res.json({
 			login: true,
 			data: decode,
 		});
 	} else {
-
-		// Return response with error
 		res.json({
 			login: false,
 			data: "error",
 		});
 	}
 });
-
 app.post('/welcome',(req, res) => {
 	res.redirect("/welcome")
 	});
@@ -144,8 +95,6 @@ app.post('/login',(req, res) => {
 app.post('/signup',(req, res) => {
 	res.redirect("/signup")
 	});
-
-// Listen the server
 app.listen(port, () => {
 	console.log(`Server is running : 
 	http://localhost:${port}/`);
